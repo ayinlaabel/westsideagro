362604482842d76a3b1d192563797112aa368b77
ayinlaabel <ayinlaabel@gmail.com>
2021-04-13 16:32:50 +0100

    Contact Page and About Page Creation but not responsive

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,26 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { 
+                        title: 'Home',
+                        home: 'active',
+                        about:'',
+                        contact: '' });
+});
+
+router.get('/contact-us', function(req, res, next) {
+  res.render('contact', { 
+                        title: 'Contact',
+                        contact: 'active',
+                        about:'',
+                        home: '' });
+});
+router.get('/about-us', function(req, res, next) {
+  res.render('about', { 
+                        title: 'About',
+                        about: 'active',
+                        contact:'',
+                        home: '' });
 });
 
 module.exports = router;
